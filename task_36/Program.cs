a074cdb9315cad01601d656473994b4168c92d2e
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-28 09:28:11 +0300

    задача вторая

---
+++ task_36/Program.cs
@@ -0,0 +1,16 @@
+﻿Random random = new Random();
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(1, 101);
+    Console.Write(array[i] + " ");
+}
+
+int sum = 0;
+for (int i = 1; i < array.Length; i += 2)
+{
+    sum += array[i];
+}
+
+Console.WriteLine("\nСумма элементов с нечетными индексами: " + sum);
