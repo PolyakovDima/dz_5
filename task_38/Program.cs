6b2d69b61b8d6409f19f363a42a7bed88b3de1ed
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-28 09:32:35 +0300

    задача третья

---
+++ task_38/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] array = { 3.14, -2.71, 1.618, 0, 2.718, -0.577, 1 };
+double max = double.MinValue;
+double min = double.MaxValue;
+
+foreach (double number in array)
+{
+    if (number > max)
+    {
+        max = number;
+    }
+    if (number < min)
+    {
+        min = number;
+    }
+}
+
+double diff = max - min;
+Console.WriteLine($"Разница между максимальным ({max}) и минимальным ({min}) элементами массива: {diff}");
