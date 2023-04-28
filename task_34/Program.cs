9249ae0011d46fe6b8e6e511d457ac3f1863d862
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-28 09:22:05 +0300

    первая задача

---
+++ task_34/Program.cs
@@ -0,0 +1,16 @@
+﻿Random random = new Random();
+ushort[] array = new ushort[8];
+int evenCount = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = (ushort)random.Next(100, 1000); // случайное трехзначное число
+    Console.Write($"{array[i]} ");
+
+    if (array[i] % 2 == 0) // проверка на четность
+    {
+        evenCount++;
+    }
+}
+
+Console.WriteLine($"\nКоличество четных чисел: {evenCount}");
